6847310874fd4fa4d055dcf33a0d023e0ad9f146
Alex Jordan <alexander3223098@gmail.com>
2013-08-17 20:01:52 -0400

    add wip setup.py for libtemplater

---
+++ libtemplater/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='templater',
+	version='0.1',
+	description='Mustache for the masses.',
+	author='Alex Jordan',
+	author_email='alexander3223098@gmail.com',
+	url='https://github.com/strugee/templater',
+	py_modules=['templater'],
+	
+     )
